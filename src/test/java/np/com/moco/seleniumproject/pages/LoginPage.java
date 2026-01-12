f74af005300a283af7de7bf09a8f06427daed67f
[sandesh thapa] <[sandeshthapa017@gmail.com]>
2026-01-12 13:36:43 +0545

    added assetions

--- src/test/java/np/com/moco/seleniumproject/pages/LoginPage.java
+++ src/test/java/np/com/moco/seleniumproject/pages/LoginPage.java
@@ -3,9 +3,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package np.com.moco.seleniumproject.pages;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+// import dev.failsafe.internal.util.Assert;
+import np.com.moco.seleniumproject.utils.WaitUtils;
+import np.com.moco.seleniumproject.utils.screenshots;
+import org.testng.Assert; // TestNG assertions
 
 /**
  *
@@ -17,6 +22,12 @@ public class LoginPage {
     private final By username = By.id("login_user");
     private final By password = By.id("login_password");
     private final By loginBtn = By.xpath("//button[contains(@class,'btn-primary')]");
+    private static final By dashboardHeader = By.xpath("//*[@id='app']/div/div[2]/div[2]/div/div[1]/div/div/h3/strong"); // By:
+                                                                                                                         // XPath
+                                                                                                                         // pointing
+                                                                                                                         // to
+                                                                                                                         // dashboard
+                                                                                                                         // heading
 
     public LoginPage(WebDriver driver) {
         this.driver = driver;
@@ -27,9 +38,8 @@ public class LoginPage {
         driver.findElement(password).sendKeys(pass);
         driver.findElement(loginBtn).click();
     }
-    
-    public boolean isUserDisplayed(){
+
+    public boolean isUserDisplayed() {
         return driver.getPageSource().contains(" Welcome, Vivek");
     }
 }
-
--- src/test/java/np/com/moco/seleniumproject/tests/user/LoginPageTest.java
+++ src/test/java/np/com/moco/seleniumproject/tests/user/LoginPageTest.java
@@ -1,42 +1,53 @@
-
 package np.com.moco.seleniumproject.tests.user;
 
 import np.com.moco.seleniumproject.base.BaseTest;
 import np.com.moco.seleniumproject.pages.LoginPage;
 import np.com.moco.seleniumproject.assertions.LoginAssertions;
-import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginPageTest extends BaseTest {
 
     @Test
     public void testSuccessfulLogin() throws InterruptedException {
-        
         LoginPage loginPage = new LoginPage(driver);
         // Reading values from config.properties via the 'config' object in BaseTest
         loginPage.login(config.getProperty("username"), config.getProperty("password"));
-        
-        //Assertion check if dashboard is loaded
+
+        // Assertion check if dashboard is loaded
         LoginAssertions.assertUserLogged(driver);
     }
-    
+
     @Test
     public void testEmptyEmail() throws InterruptedException {
-        
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login("", config.getProperty("password"));
+        // Verify user is not logged in and appropriate error is shown
+        Assert.assertFalse(loginPage.isUserDisplayed(), "User should not be logged in with empty email");
+        LoginAssertions.assertEmailErrorIsDisplayed(driver);
     }
-    
+
     @Test
     public void testEmptyPass() throws InterruptedException {
-        
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login(config.getProperty("username"), "");
+        // Verify user is not logged in when password is empty
+        Assert.assertFalse(loginPage.isUserDisplayed(), "User should not be logged in with empty password");
     }
-    
+
     @Test
     public void testInvalidEmail() throws InterruptedException {
-        
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login("invalid@example.com", config.getProperty("password"));
+        // Verify invalid credentials do not allow login
+        Assert.assertFalse(loginPage.isUserDisplayed(), "User should not be logged in with invalid email");
     }
-    
+
     @Test
     public void testEmptyPassword() throws InterruptedException {
-        
+        LoginPage loginPage = new LoginPage(driver);
+        // both fields empty
+        loginPage.login("", "");
+        Assert.assertFalse(loginPage.isUserDisplayed(), "User should not be logged in with empty credentials");
     }
 }
