80a598161f2f006f7828b491c420ed3013aed145
unknown <learner.bibekdas@gmail.com>
2026-01-13 12:04:12 +0545

    Operator pages created

---
+++ src/test/java/np/com/moco/seleniumproject/pages/OperatorPage.java
@@ -0,0 +1,25 @@
+package np.com.moco.seleniumproject.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+
+
+public class OperatorPage {
+    private final WebDriver driver;
+    private final By addbutton = By.xpath("//*[@id=\"app\"]/div/div[2]/div[2]/div/div[1]/div/div/div[2]/a/svg/path");
+    private final By fullName = By.xpath("//input[@placeholder=\"Full Name\"]");
+    private final By emailAddress = By.xpath("//input[@placeholder=\"Email Address\"]");
+    private final By contactNumber = By.xpath("//input[@placeholder=\"Contact Number\"]");
+    private final By organization = By.xpath("//select[@class=\"form-select\"]");
+    
+    public OperatorPage(WebDriver driver) {
+        this.driver = driver;
+    }
+    
+    
+    
+    
+    
+    
+}
