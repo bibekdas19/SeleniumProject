03ea0015f968b19eb831395da54eda32001c370b
unknown <learner.bibekdas@gmail.com>
2026-01-13 12:41:39 +0545

    add operator form

--- src/test/java/np/com/moco/seleniumproject/pages/OperatorPage.java
+++ src/test/java/np/com/moco/seleniumproject/pages/OperatorPage.java
@@ -2,6 +2,8 @@ package np.com.moco.seleniumproject.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 
 
@@ -11,13 +13,20 @@ public class OperatorPage {
     private final By fullName = By.xpath("//input[@placeholder=\"Full Name\"]");
     private final By emailAddress = By.xpath("//input[@placeholder=\"Email Address\"]");
     private final By contactNumber = By.xpath("//input[@placeholder=\"Contact Number\"]");
-    private final By organization = By.xpath("//select[@class=\"form-select\"]");
+    private final By organi = By.xpath("//select[@class=\"form-select\"]");
     
     public OperatorPage(WebDriver driver) {
         this.driver = driver;
     }
     
-    public void AddOperator(String name, String email,String contact,String contract,String organization){
+    public void AddOperator(String name, String email,String contact,String contract){
+        driver.findElement(addbutton).click();
+        driver.findElement(fullName).sendKeys("Sandesh Thapa Magar");
+        driver.findElement(emailAddress).sendKeys("sandeshthapa@moco.com.np");
+        driver.findElement(contactNumber).sendKeys("9849719431");
+        WebElement ddown = driver.findElement(organi);
+        Select select = new Select(ddown);
+        select.selectByValue("MOCO");
         
     }
     
